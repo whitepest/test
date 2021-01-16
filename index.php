b4174dd292a6b726e0f2c2a7e861477f298158fa
kozakotomato <68596676+KoZaKotomato@users.noreply.github.com>
2021-01-16 23:22:08 +0200

    story 2: branch of application logic

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+
+
+$i = 1 + 1;
+
+if ($i):
+    $body = "<h1 class='blue'>$i</h1>";
+else:
+    $body = "<h1 class='red'>$i</h1>";
+endif;
+
+$tpl = file_get_contents("template.html");
+
+$tpl = str_replace('{body}', $body, $tpl);
+
+echo $tpl;
