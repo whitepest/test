4256a0cb266cae03b04fb144eb3f218fc57e293d
kozakotomato <68596676+KoZaKotomato@users.noreply.github.com>
2021-01-17 12:26:44 +0200

    story 3: removal of excess

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
 <?php
 
 
-$i = 1 + 1;
-
-if ($i):
-    $body = "<h1 class='blue'>$i</h1>";
-else:
-    $body = "<h1 class='red'>$i</h1>";
-endif;
-
-$tpl = file_get_contents("template.html");
-
-$tpl = str_replace('{body}', $body, $tpl);
-
-echo $tpl;
