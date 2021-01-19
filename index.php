c436bf255c2d2d51849316a67099170f08f2cd02
kozakotomato <68596676+KoZaKotomato@users.noreply.github.com>
2021-01-19 18:10:19 +0200

    story 9: mysql connection and charset utf8

--- index.php
+++ index.php
@@ -5,4 +5,13 @@ function html($text) {
     return htmlspecialchars($text, ENT_QUOTES);
 }
 
+$link = mysqli_connect("localhost", "root", "root");
+
+if ($link == false):
+    print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
+    exit();
+endif;
+
+mysqli_set_charset($link, "utf8");
+
 include ("template/outPut.html");
